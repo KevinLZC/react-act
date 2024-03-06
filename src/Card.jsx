b14828c1bbf24b064ca6783214a56cda465c4f07
KevinLZC <kevin_lazarillo@hotmail.com>
2024-03-06 15:48:54 -0600

    Visual Changes

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,28 @@
+import PropTypes from 'prop-types'
+
 export function Card(props) {
+
+  const { songImage, songName, artistName, albumName, releaseDate, listened } = props
+
   return (
     <div className="card">
-      <img src={props.songImage} alt={props.songName} />
+      <img src={songImage} alt={songName} />
       <div className="card-details">
-        <h1>{props.songName}</h1>
-        <p>{props.artistName}</p>
-        <p>{props.albumName}</p>
-        <p>{props.releaseDate}</p>
-        {props.listened ? <p style={{color: "green"}}>Listened</p> : <p style={{color: "red"}}>Not Listened</p>}
+        <h1> {songName}</h1>
+        <p>Artist: {artistName}</p>
+        <p>Album: {albumName}</p>
+        <p>Release: {releaseDate}</p>
+        {listened ? <p style={{ color: "green" }}>Listened</p> : <p style={{ color: "red" }}>Not Listened</p>}
       </div>
     </div>
-  );
+  )
+}
+
+Card.propTypes = {
+  songImage: PropTypes.string.isRequired,
+  songName: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired,
+  albumName: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired,
+  listened: PropTypes.bool.isRequired
 }
