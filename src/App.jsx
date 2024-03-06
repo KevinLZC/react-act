44686d95687e53b8af951ea57c620f83d004fd30
ricardocastellr <rcastell@ucol.mx>
2024-03-05 22:09:55 -0600

    Add style to card and add new font

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import { Card } from './Card'
+import '../fonts/Geist/Geist-Regular.otf';
 
 const songs = [
   {
-    songImage: '',
-    songName: 'Europa',
-    artistName: 'Santana',
-    albumName: 'Amigos',
-    releaseDate: '1976',
+    songImage: 'https://i.scdn.co/image/ab67616d0000b273d13ca3ed0b13d9038667a536',
+    songName: 'Uranium Fever',
+    artistName: 'Elton Britt',
+    albumName: 'Uranium Rock - Fallout 4 Radio',
+    releaseDate: '1955',
     listened: false
   },
   {
-    songImage: '',
-    songName: "Flor D'Luna",
-    artistName: 'Santana',
-    albumName: "Flor D'Luna",
-    releaseDate: '1977',
+    songImage: 'https://i.scdn.co/image/ab67616d0000b2732c81905d4a216deda92d91ae',
+    songName: "Atomb Bomb Baby",
+    artistName: 'The Five Stars',
+    albumName: "Atomb Bomb Baby",
+    releaseDate: '1957',
     listened: true
   },
   {
@@ -24,7 +25,7 @@ const songs = [
     songName: 'Europa',
     artistName: 'Santana',
     albumName: 'Amigos',
-    releaseDate: '1976-03-26',
+    releaseDate: '1976',
     listened: false
   },
 ]
@@ -36,9 +37,9 @@ function App() {
 
   return (
     <>
-      <h1>Santana Songs</h1>
-      <img src={'https://i.scdn.co/image/ab6761610000e5eb09882b1b7b33732abd60fc38'} alt="Carlos Santana" />
-      <div>
+      <h1>Music playlist</h1>
+      <img src={'https://alfinaleslodesiempre.files.wordpress.com/2018/04/cropped-sombrero-de-caca-1.png'} alt="Carlos Santana" />
+      <div className='checkbox'>
         <label>
           <input
             type="checkbox"
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,14 @@
-
 export function Card(props) {
   return (
-    <>
-      <div>
-        <img src={props.songImage}></img>
-        <div>
-          <h1>{props.songName}</h1>
-          <p>{props.artistName}</p>
-          <p>{props.albumName}</p>
-          <p>{props.releaseDate}</p>
-          {props.listened ? <p>Listened</p> : <p>Not Listened</p>}
-        </div>
+    <div className="card">
+      <img src={props.songImage} alt={props.songName} />
+      <div className="card-details">
+        <h1>{props.songName}</h1>
+        <p>{props.artistName}</p>
+        <p>{props.albumName}</p>
+        <p>{props.releaseDate}</p>
+        {props.listened ? <p style={{color: "green"}}>Listened</p> : <p style={{color: "red"}}>Not Listened</p>}
       </div>
-    </>
-  )
+    </div>
+  );
 }
